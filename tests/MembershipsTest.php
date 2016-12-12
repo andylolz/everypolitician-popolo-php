06e01a754df5a899a97ebefc53f21b551db94d22
Andy Lulham <a.lulham@gmail.com>
2016-12-12 00:32:26 +0000

    Add some more membership tests

--- tests/MembershipsTest.php
+++ tests/MembershipsTest.php
@@ -87,6 +87,49 @@ NOW;
 }
 NOW;
 
+    const EXAMPLE_MULTIPLE_MEMBERSHIPS = <<<'NOW'
+{
+    "persons": [
+        {
+            "id": "SP-937-215",
+            "name": "Jean-Luc Picard"
+        },
+        {
+            "id": "SC-231-427",
+            "name": "William Riker"
+        }
+    ],
+    "organizations": [
+        {
+            "id": "starfleet",
+            "name": "Starfleet"
+        },
+        {
+            "id": "gardening-club",
+            "name": "Boothby's Gardening Club"
+        }
+    ],
+    "memberships": [
+        {
+            "person_id": "SP-937-215",
+            "organization_id": "starfleet",
+            "start_date": "2327-12-01"
+        },
+        {
+            "person_id": "SP-937-215",
+            "organization_id": "gardening-club",
+            "start_date": "2323-01-01",
+            "end_date": "2327-11-31"
+        },
+        {
+            "person_id": "SC-231-427",
+            "organization_id": "starfleet",
+            "start_date": "2357-03-08"
+        }
+    ]
+}
+NOW;
+
     public function testEmptyFileGivesNoMemberships()
     {
         $filename = $this->exampleFile('{}');
@@ -195,5 +238,143 @@ NOW;
         $this->assertCount(1, $popoloB->memberships);
         $mB = $popoloB->memberships[0];
         $this->assertTrue($mA->equals($mB));
+    }
+
+    public function testPersonMembershipsMethod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MULTIPLE_MEMBERSHIPS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $person = $popolo->persons->first;
+        $personMemberships = $person->memberships;
+        $this->assertCount(2, $personMemberships);
+        $this->assertTrue($popolo->memberships[0]->equals($personMemberships[0]));
+        $this->assertTrue($popolo->memberships[1]->equals($personMemberships[1]));
+    }
+
+    public function testMembershipPersonMethod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MULTIPLE_MEMBERSHIPS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $personPicard = $popolo->persons[0];
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->person->equals($personPicard));
+    }
+
+    public function testMembershipOrganizationMethod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MULTIPLE_MEMBERSHIPS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $orgStarfleet = $popolo->organizations->first;
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->organization->equals($orgStarfleet));
+    }
+
+    public function testMembershipOnBehalfOfMethod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $orgAdderParty = $popolo->organizations[1];
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->onBehalfOf->equals($orgAdderParty));
+    }
+
+    public function testMembershipAreaMethod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $areaDunny = $popolo->areas[0];
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->area->equals($areaDunny));
+    }
+
+    public function testMembershipPostMethod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $postDunny = $popolo->posts[0];
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->post->equals($postDunny));
+    }
+
+    public function testMembershipLegislativePeriod()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $eventPitt = $popolo->events[0];
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->legislativePeriod->equals($eventPitt));
+    }
+
+    public function testMembershipCurrentAtTrue()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $m = $popolo->memberships[0];
+        $this->assertTrue($m->currentAt(new \DateTime('1784-04-30')));
+    }
+
+    public function testMembershipCurrentAtFalseBefore()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $m = $popolo->memberships[0];
+        $this->assertFalse($m->currentAt(new \DateTime('1600-01-01')));
+    }
+
+    public function testMembershipCurrentAtFalseAfter()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $m = $popolo->memberships[0];
+        $this->assertFalse($m->currentAt(new \DateTime('1800-01-01')));
+    }
+
+    // public function testMembershipCurrentTrue()
+    // {
+    //     // TODO: mock today's date to date(1784, 4, 30)
+    //     $filename = $this->exampleFile(self::EXAMPLE_MEMBERSHIP_ALL_FIELDS);
+    //     $popolo = Popolo::fromFilename($filename);
+    //     unlink($filename);
+    //
+    //     $m = $popolo->memberships[0];
+    //     $this->assertTrue($m->current);
+    // }
+
+    public function testHashMagicMethod()
+    {
+        // Not sure how to implement this test in PHP!
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testEqualityAndInequalityNotImplemented()
+    {
+        $filename = $this->exampleFile(self::EXAMPLE_MULTIPLE_MEMBERSHIPS);
+        $popolo = Popolo::fromFilename($filename);
+        unlink($filename);
+
+        $m = $popolo->memberships->first;
+        $this->assertFalse($m->equals('a string, not a person'));
     }
 }
